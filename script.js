87be1c0b691cf387bc91437e8bcb773dea8f9a9d
Bruno <bdorigon@hotmail.com>
2023-07-22 22:39:49 -0300

    add-animation

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-const div = document.querySelector(".cores");
-const button = document.querySelector("button");
+function generateColor() {
+  const div = document.querySelector(".cores");
+  const button = document.querySelector("button");
 
-function handleClick() {
-  div.innerHTML = "";
+  function handleClick() {
+    div.innerHTML = "";
 
-  for (let i = 0; i < 4; i++) {
-    const color = randomColor();
-    const divElement = document.createElement("div");
-    const elementP = document.createElement("p");
+    for (let i = 0; i < 4; i++) {
+      const color = randomColor();
+      const divElement = document.createElement("div");
+      const elementP = document.createElement("p");
 
-    divElement.style.background = color;
-    elementP.style.color = color;
-    elementP.textContent = color;
+      divElement.style.background = color;
+      elementP.style.color = color;
+      elementP.textContent = color;
 
-    divElement.appendChild(elementP);
-    div.appendChild(divElement);
+      divElement.appendChild(elementP);
+      div.appendChild(divElement);
+    }
   }
-}
 
-function randomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
+  function randomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
 
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
-  return color;
-}
 
-button.addEventListener("click", handleClick);
+  button.addEventListener("click", handleClick);
+}
+generateColor();
